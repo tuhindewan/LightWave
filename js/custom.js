93320fffe862eb4dfa93179a57cbf9b20c4498d8
tuhindewan <tuhinsshadow@gmail.com>
2017-01-13 22:51:29 -0800

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+$.vegas('slideshow', {
+backgrounds: [
+{ src: 'img/1.jpg', fade: 1000, delay: 9000 },
+{ src: 'img/2.jpg', fade: 1000, delay: 9000 }
+]
+});
+
+$('#myCarousel').carousel({
+interval: 2000 
+});
+
+$('.scroll-menu a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+var $anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $($anchor.attr('href')).offset().top
+}, 1200, 'easeInOutExpo');
+event.preventDefault();
+});
+
+
+});
